43543f89b6d32cc8381ad0c4990364e838bdeba1
Spandan Saxena <spandansaxena@gmail.com>
2021-04-23 21:13:47 +0530

    Added Splash Screen
    The right way!

--- app/src/main/java/com/onesilicondiode/dropme/Landing.java
+++ app/src/main/java/com/onesilicondiode/dropme/Landing.java
@@ -1,14 +1,14 @@
 package com.onesilicondiode.dropme;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class Landing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing);
     }
 }
---
+++ app/src/main/java/com/onesilicondiode/dropme/LaunchScreen.java
@@ -0,0 +1,16 @@
+package com.onesilicondiode.dropme;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(LaunchScreen.this, Landing.class));
+        finish();
+    }
+}
