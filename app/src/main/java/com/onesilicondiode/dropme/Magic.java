21af8f2ba790fbfb9816974e7618aa821ddf056a
Spandan Saxena <spandansaxena@gmail.com>
2021-04-25 21:50:14 +0530

    Added Activity where magic is to happen

---
+++ app/src/main/java/com/onesilicondiode/dropme/Magic.java
@@ -0,0 +1,14 @@
+package com.onesilicondiode.dropme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Magic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magic);
+    }
+}
