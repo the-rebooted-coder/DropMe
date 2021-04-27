06ee83e5b6d5c4d4b0faf9ed04cfd9def9cec61f
Spandan Saxena <spandansaxena@gmail.com>
2021-04-27 17:59:43 +0530

    No Internet Check Added

--- app/src/main/java/com/onesilicondiode/dropme/NoInternet.java
+++ app/src/main/java/com/onesilicondiode/dropme/NoInternet.java
@@ -4,22 +4,30 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LaunchScreen extends AppCompatActivity {
+public class NoInternet extends AppCompatActivity {
+    Button retry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(haveNetwork()){
-            startActivity(new Intent(LaunchScreen.this, Magic.class));
-            finish();
-        }
-        else {
-            startActivity(new Intent(LaunchScreen.this, NoInternet.class));
-            finish();
-        }
+        setContentView(R.layout.activity_no_internet);
+        retry = findViewById(R.id.retryConnection);
+        retry.setOnClickListener(v -> {
+            if (haveNetwork()){
+                Toast.makeText(NoInternet.this,"Wohoooooooo!",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(NoInternet.this, Magic.class));
+                finish();
+            }
+            else {
+                Toast.makeText(NoInternet.this,"Nope, Still no Luck!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     //Network Checking Boolean
     private boolean haveNetwork() {
