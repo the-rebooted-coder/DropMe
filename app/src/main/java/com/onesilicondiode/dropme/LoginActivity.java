218ed7adab652b1d2cf4aa9124003a49fc1eaccc
Spandan Saxena <spandansaxena@gmail.com>
2021-04-28 21:19:02 +0530

    Google Login Initialisation

--- app/src/main/java/com/onesilicondiode/dropme/LoginActivity.java
+++ app/src/main/java/com/onesilicondiode/dropme/LoginActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
     }
 }
