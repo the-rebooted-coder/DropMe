d0adff429ccd6ea6a03c5d916c59eb2e196ba112
Shrish Sharma <shrish0608@gmail.com>
2021-04-29 13:42:56 +0530

    SignOut Test 2

--- docs/signout.js
+++ docs/signout.js
@@ -1,3 +1,7 @@
 function googleSignOut() {
-    firebase.auth().signoOut()
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+    }, function(error) {
+        console.error('Sign Out Error', error);
+    });
 }
