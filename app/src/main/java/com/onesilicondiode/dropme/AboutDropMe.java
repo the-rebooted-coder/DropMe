53c88c68b58e73fc8cc2510c7060ee2f0c452361
Spandan Saxena <spandansaxena@gmail.com>
2021-05-01 17:13:20 +0530

    Created Empty About App Activity

---
+++ app/src/main/java/com/onesilicondiode/dropme/AboutDropMe.java
@@ -0,0 +1,14 @@
+package com.onesilicondiode.dropme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutDropMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_drop_me);
+    }
+}
