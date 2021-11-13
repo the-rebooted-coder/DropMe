3e9eef65da8c7d2c3eb785dd5f732fe3cd90720f
Spandan Saxena <spandansaxena@gmail.com>
2021-11-13 11:32:11 +0530

    testRunner

--- app/src/main/java/com/onesilicondiode/dropme/App.kt
+++ app/src/main/java/com/onesilicondiode/dropme/App.kt
@@ -3,6 +3,17 @@ package com.onesilicondiode.dropme
 import android.app.Application
 import com.theapache64.removebg.RemoveBg
 
+@Runwith(JUnit4::class)
+class MainActivtyTest{
+    @get:Rule
+    val IntentarestRule = IntenterestRule(mainactivity.class)
+            @Test fun changestestnewactivity() vat resultDate = Intent() val erdTert 'Spandan'
+    ResultData.putExtra(sweeLname., entlText I val result =Instrumentatlan.ActIvItyResultfactivity.REsultoK, result Data)
+    intenetrayftonackage(SecandActivity:sclass.simplename)).respondaith(result) closeSoftlreykroartlf)
+}
+
+
+
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
